aa7a14235cc84e9c839f4e02311e2fa9c90d841a
Rooz K <roozforeverrr@gmail.com>
2019-07-26 18:11:41 +0200

    attacking movement+animation

--- BPW_Blok4/Assets/Script/PlayerMovement.cs
+++ BPW_Blok4/Assets/Script/PlayerMovement.cs
@@ -2,20 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum PlayerState // als een bool met meer functies
+{
+    walk,
+    attack,
+    interact
+}
+
+
 public class PlayerMovement : MonoBehaviour
 {
     // Start is called before the first frame update
+    public PlayerState currentState;
     public float speed;
     private Rigidbody2D PlayerRB;
     private Vector3 move;
-
     private Animator animator;
 
 
     void Start()
     {
+        currentState = PlayerState.walk;
         animator = GetComponent<Animator>();
         PlayerRB = GetComponent<Rigidbody2D>();
+        animator.SetFloat("moveX", 0);
+        animator.SetFloat("moveY", -1);
 
     }
 
@@ -25,7 +36,31 @@ public class PlayerMovement : MonoBehaviour
         move = Vector3.zero;
         move.x = Input.GetAxisRaw("Horizontal");
         move.y = Input.GetAxisRaw("Vertical");
-        if(move != Vector3.zero)
+        if(Input.GetButtonDown("Space") && currentState != PlayerState.attack)
+        {
+            StartCoroutine(AttackCo());
+        }
+        else if (currentState == PlayerState.walk)
+        {
+            UpdateAnimationAndMove();
+        }
+
+    }
+
+    private IEnumerator AttackCo()
+    {
+        animator.SetBool("attacking", true);
+        currentState = PlayerState.attack;
+        yield return null;
+        animator.SetBool("attacking", false);
+        yield return new WaitForSeconds(.2f);
+        currentState = PlayerState.walk;
+    }
+
+
+    void UpdateAnimationAndMove()
+    {
+        if (move != Vector3.zero)
         {
             MoveCharacter();
             animator.SetFloat("moveX", move.x);
@@ -38,7 +73,6 @@ public class PlayerMovement : MonoBehaviour
         }
 
     }
-
     
 
     void MoveCharacter()
