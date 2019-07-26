599e4297b3be9c60f19a9cb1bb4cb9727ddd37d8
Rooz K <roozforeverrr@gmail.com>
2019-07-26 19:22:49 +0200

    Breakable object script

---
+++ BPW_Blok4/Assets/Script/Destroy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour
+{
+    private Animator breakanim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        breakanim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Breaking()
+    {
+        breakanim.SetBool("break", true);
+        Destroy(gameObject);
+    }
+}
+ 
